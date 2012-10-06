c7100b22bf111d33d265822813c2ed6250dbbf1f
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-10-06 10:30:43 +0900

    Update code and setting to deploy to heroku

--- Application.hs
+++ Application.hs
@@ -16,6 +16,12 @@ import qualified Database.Persist.Store
 import Database.Persist.GenericSql (runMigration)
 import Network.HTTP.Conduit (newManager, def)
 
+import Data.HashMap.Strict as M
+import Data.Aeson.Types as AT
+#ifndef DEVELOPMENT
+import qualified Web.Heroku
+#endif
+
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
 import Handler.Home
@@ -42,12 +48,34 @@ makeFoundation :: AppConfig DefaultEnv Extra -> IO App
 makeFoundation conf = do
     manager <- newManager def
     s <- staticSite
+    hconfig <- loadHerokuConfig
     dbconf <- withYamlEnvironment "config/postgresql.yml" (appEnv conf)
-              Database.Persist.Store.loadConfig >>=
+              (Database.Persist.Store.loadConfig . combineMappings hconfig) >>=
               Database.Persist.Store.applyEnv
     p <- Database.Persist.Store.createPoolConfig (dbconf :: Settings.PersistConfig)
     Database.Persist.Store.runPool dbconf (runMigration migrateAll) p
     return $ App conf s p manager dbconf
+
+#ifndef DEVELOPMENT
+canonicalizeKey :: (Text, val) -> (Text, val)
+canonicalizeKey ("dbname", val) = ("database", val)
+canonicalizeKey pair = pair
+
+toMapping :: [(Text, Text)] -> AT.Value
+toMapping xs = AT.Object $ M.fromList $ map (\(key, val) -> (key, AT.String val)) xs
+#endif
+
+combineMappings :: AT.Value -> AT.Value -> AT.Value
+combineMappings (AT.Object m1) (AT.Object m2) = AT.Object $ m1 `M.union` m2
+combineMappings _ _ = error "Data.Object is not a Mapping."
+
+loadHerokuConfig :: IO AT.Value
+loadHerokuConfig = do
+#ifdef DEVELOPMENT
+  return $ AT.Object M.empty
+#else
+  Web.Heroku.dbConnParams >>= return . toMapping . map canonicalizeKey
+#endif
 
 -- for yesod devel
 getApplicationDev :: IO (Int, Application)
