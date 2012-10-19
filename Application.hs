417920d34c167379b8886c914635b56864b4599c
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-10-20 02:43:58 +0900

    Add blog example from http://yannesposito.com/Scratch/en/blog/Yesod-tutorial-for-newbies/

--- Application.hs
+++ Application.hs
@@ -19,6 +19,7 @@ import Network.HTTP.Conduit (newManager, def)
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
 import Handler.Home
+import Handler.Blog
 
 import Data.HashMap.Strict as M
 import Data.Aeson.Types as AT
---
+++ Handler/Blog.hs
@@ -0,0 +1,38 @@
+module Handler.Blog where
+
+import Import
+import Data.Monoid
+
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+instance YesodNic App
+
+entryForm :: Form Article
+entryForm = renderDivs $ Article
+    <$> areq textField "Title" Nothing
+    <*> areq nicHtmlField "Content" Nothing
+
+getBlogR :: Handler RepHtml
+getBlogR = do
+  articles <- runDB $ selectList [] [Desc ArticleTitle]
+  (articleWidget, enctype) <- generateFormPost entryForm
+  defaultLayout $ do
+    $(widgetFile "articles")
+
+postBlogR :: Handler RepHtml
+postBlogR = do
+  ((res, articleWidget), enctype) <- runFormPost entryForm
+  case res of
+    FormSuccess article -> do
+      articleId <- runDB $ insert article
+      setMessage $ toHtml $ (articleTitle article) <> " created"
+      redirect $ ArticleR articleId
+    _ -> defaultLayout $ do
+      setTitle "Please correct your entry form"
+      $(widgetFile "articleAddError")
+
+getArticleR :: ArticleId -> Handler RepHtml
+getArticleR articleId = do
+  article <- runDB $ get404 articleId
+  defaultLayout $ do
+    setTitle $ toHtml $ articleTitle article
+    $(widgetFile "article")
